6a5b9c8c20f7fb19ae93ba2772fb59ae4a875f3f
miqueiasdecco <102546355+miqueiasdecco@users.noreply.github.com>
2023-08-22 19:52:59 +0000

    Adicionando pages1

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home () {
+    return (
+        <div>
+            <h1>Fabi, eu te amo muito.</h1>
+            <p> Nesses anos de casado sou muito feliz ao seu lado 😍🥰🥰❤️❤️  </p>
+            <p>Tenho certeza de que seu negócio vai dar muito certo. Você é muito capaz.</p>
+            <p> Se vc gostou dá uma risadinha </p>
+        </div>
+    )
+}
+
+export default Home
